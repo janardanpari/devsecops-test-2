f7039d8899c7ec5289631638617265f55b46ace3
Janardan Pari <77274406+janardanpari@users.noreply.github.com>
2022-07-20 16:17:52 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -3,6 +3,7 @@
 OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
 REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directories"
@@ -12,15 +13,19 @@ if [ ! -d "$DATA_DIRECTORY" ]; then
     mkdir -p "$REPORT_DIRECTORY"
     chmod -R 777 "$REPORT_DIRECTORY"
 fi
+if [ ! -d "$CACHE_DIRECTORY" ]; then
+    echo "Initially creating persistent directory: $CACHE_DIRECTORY"
+    mkdir -p "$CACHE_DIRECTORY"
+fi
 
 # Make sure we are using the latest version
-docker pull owasp/dependency-check
+docker pull owasp/dependency-check:$DC_VERSION
 
 docker run --rm \
-    --volume $(pwd):/src \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
-    --volume "$REPORT_DIRECTORY":/report \
-    owasp/dependency-check \
+    --volume $(pwd):/src:z \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
+    --volume "$REPORT_DIRECTORY":/report:z \
+    owasp/dependency-check:$DC_VERSION \
     --scan /src \
     --format "ALL" \
     --project "My OWASP Dependency Check Project" \
